7880787f55a260d3d5f6cb53a49ef2ac56c33030
THANGNDFPL <nguyenthang070103@gmail.com>
2023-02-12 09:35:29 +0700

    BT11/02

--- src/screens/Products/index.js
+++ src/screens/Products/index.js
@@ -9,29 +9,31 @@ export default function Products(props) {
 
     useEffect(() => {
         fetch('http://10.0.2.2:3000/products')
-            .then((res) => res.json())
-            .then((data) => setList(data))
-            .catch(function(error) {
+            .then(res => res.json())
+            .then(data => setList(data))
+            .catch((error) => {
                 console.log(error.message);
                 throw error;
             });
     }, [list]);
     return (
-
-        <FlatList
-            data={list}
-            renderItem={({ item }) => (
-                <View>
-                    <Text>ID: {item.id}</Text>
-                    <Text>Name: {item.name}</Text>
-                    <Text>Desc: {item.desc}</Text>
-                    <Text>Price: {item.price}</Text>
-                    <Text>Sale: {item.sale_price}</Text>
-                    <Text>Status: {item.status}</Text>
-                </View>
-            )}
-            keyExtractor={(item) => item.id}
-        />
+        <View>
+            
+            <FlatList
+                data={list}
+                renderItem={({ item }) => (
+                    <View>
+                        <Text>ID: {item.id}</Text>
+                        <Text>Name: {item.name}</Text>
+                        <Text>Desc: {item.desc}</Text>
+                        <Text>Price: {item.price}</Text>
+                        <Text>Sale: {item.sale_price}</Text>
+                        <Text>Status: {item.status}</Text>
+                    </View>
+                )}
+                keyExtractor={(item) => item.id}
+            />
+        </View>
     )
 
 }
